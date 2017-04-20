0e56bed7b17821935a2feb2f8d999e4f91cea191
Rik Smith-Unna <rik@fathomlabs.io>
2017-04-20 11:47:34 +0300

    Push last nights updates

--- app/views/result.js
+++ app/views/result.js
@@ -1,5 +1,35 @@
 var html = require('choo/html')
 
+const updateid = update => {
+  const idtype = update.doi ? 'DOI' : 'PMID'
+
+  return html`
+
+  <tr><td class="field">Update ${idtype}:</td><td>${update.doi || update.pmid}</td></tr>
+
+  `
+}
+
+const updatelink = update => {
+  if (update.doi) {
+    return html`
+
+    <a href="http://doi.org/${update.doi}">http://doi.org/${update.doi}</a>
+
+    `
+  } else {
+    return html`
+
+    <a href="http://europepmc.org/abstract/MED/${update.pmid}">http://europepmc.org/abstract/MED/${update.pmid}</a>
+
+    `
+  }
+}
+
+const timestamp = update => {
+  return update.timestamp ? (new Date(update.timestamp)).toLocaleString() : 'unknown'
+}
+
 module.exports = (state, emit) => {
   if (state.error) {
     return require('./error')()
@@ -29,13 +59,13 @@ module.exports = (state, emit) => {
           <tbody>
             <tr><td class="field">Title:</td><td>${state.result.title}</td></tr>
             <tr><td class="field">Journal:</td><td>${state.result.journal}</td></tr>
-            <tr><td class="field">Publisher:</td><td>${state.result.publisher}</td></tr>
+            <tr><td class="field">Publisher:</td><td>${state.result.publisher || 'unknown'}</td></tr>
             <tr><td class="field">Article DOI:</td><td>${state.result.doi}</td></tr>
             <tr><td class="field">Article link:</td><td><a href="http://doi.org/${state.result.doi}">http://doi.org/${state.result.doi}</a></td></tr>
-            <tr><td class="field">Update DOI:</td><td>${state.result.update.doi}</td></tr>
-            <tr><td class="field">Update link:</td><td><a href="http://doi.org/${state.result.update.doi}">http://doi.org/${state.result.update.doi}</a></td></tr>
+            ${updateid(state.result.update)}
+            <tr><td class="field">Update link:</td><td>${updatelink(state.result.update)}</td></tr>
             <tr><td class="field">Update type:</td><td>${state.result.update.type}</td></tr>
-            <tr><td class="field">Update date:</td><td>${(new Date(state.result.update.timestamp)).toLocaleString()}</td></tr>
+            <tr><td class="field">Update date:</td><td>${timestamp(state.result.update)}</td></tr>
           </tbody>
         </table>
       </div>
